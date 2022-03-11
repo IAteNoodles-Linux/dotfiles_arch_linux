0881c8e32ff6ed2d44e6e9ee4fb52059c076c62a
IAteNoodles-Linux <7opb228k3@mozmail.com>
2022-03-11 22:11:16 +0530

    Commit

--- .config/fish/functions/push.fish
+++ .config/fish/functions/push.fish
@@ -1,10 +1,12 @@
 function push
 set rep_path (pwd)
-if string match -q "/home/nuub/Project/IAteNoodles/School/Banking_API" $rep_path
-eval ssh-agent >> ssh_agent-tmp.sh
-echo "ssh-add ~/.ssh/IAteNoodles" >> ssh_agent-tmp.sh
-echo "git push" >> ssh_agent-tmp.sh
-sh ssh_agent-tmp.sh
+set identity $argv
+if string match -q "" $identity
+set identity "IAteNoodles"
 end
-rm -f ssh_agent-tmp.sh
+eval ssh-agent >> ssh_agent-tmp.sh
+echo ssh-add ~/.ssh/$identity >> ssh_agent-tmp.sh
+echo git push >> ssh_agent-tmp.sh
+sh ssh_agent-tmp.sh
+rm ssh_agent-tmp.sh
 end
